eacfab7eefb60e76c58b6dae3e0e8eb92cb52165
LamTranVinh <109349806+LamTranVinh@users.noreply.github.com>
2023-08-12 16:32:29 +0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "ap-northeast-2"  # Replace with your desired region
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-0c9c942bd7bf113a2"  # Replace with your desired AMI ID
+  instance_type = "t2.micro"               # Replace with your desired instance type
+
+  vpc_security_group_ids = ["sg-0afe369c8a18dcd91"]  # Replace with your security group ID
+
+  subnet_id = "subnet-0397861f148d6e68b"  # Replace with your subnet ID
+
+  key_name = "ssh-key-pem-ansible-playbook"  # Replace with your key pair name
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.my_instance.public_ip
+}
